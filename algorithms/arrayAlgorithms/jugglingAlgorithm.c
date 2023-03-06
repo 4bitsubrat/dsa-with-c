ccca17b18cd66fba465b964d2602a48f4629eead
4bitsubrat <badsubrat@gmail.com>
2023-03-06 22:08:24 +0530

    Juggling Algorithm Added

---
+++ algorithms/arrayAlgorithms/jugglingAlgorithm.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showArray(int *array, int elementsCount)
+{
+    for (int i = 0; i < elementsCount; i++)
+    {
+        printf(" %d ", array[i]);
+    }
+    printf("\n");
+}
+
+int gcd(int n, int k)
+{
+    return k == 0 ? n : gcd(k, n % k);
+}
+
+void rotateByJuggle(int *array, int n, int k)
+{
+    for (int i = 0; i < gcd(n, k); i++)
+    {
+        int j = 0;
+        int x = array[i];
+        while (1)
+        {
+            array[((i + (j * k)) % n)] = array[((i + ((j + 1) * k)) % n)];
+            j++;
+            if (((i + ((j + 1) * k)) % n) == i)
+                array[((i + j * k) % n)] = x;
+            if (((i + ((j + 1) * k)) % n) == i)
+                break;
+        }
+    }
+}
+
+int main()
+{
+    int array[] = {23, 13, 17, 39, 79, 19, 45, 37, 76, 43, 21, 65, 34, 39, 45, 13};
+    int k;
+    printf("Enter position to rotate...");
+    scanf("%d", &k);
+    printf("Array before roation...\n");
+    showArray(array, sizeof(array) / sizeof(int));
+    rotateByJuggle(array, sizeof(array) / sizeof(int), k);
+    printf("Array after roation...\n");
+    showArray(array, sizeof(array) / sizeof(int));
+    return 0;
+}
